1671fb8bd1c8a48f90bc4e4c415e9716d0cc754d
JoshPrice315 <61852564+JoshPrice315@users.noreply.github.com>
2020-06-24 17:21:20 -0700

    finished

--- models/User.js
+++ models/User.js
@@ -1,40 +1,52 @@
-const bcrypt = require('bcrypt');
+
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-// create our User model
+// create user model
 class User extends Model {
-    // set up method to run on instance data (per user) to check password
+    //set up method to run on instance data (per user) to check password
     checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
+        return bcrypt.compare(loginPw, this.password);
     }
 }
 
-// define table columns and configuration 
+// define table colums and configs
 User.init(
     {
+        // define id column
         id: {
+            // use special Sequelize Datatypes object provie what type of data it is
             type: DataTypes.INTEGER,
+            // thbis is equivalent of sqls not null option
             allowNull: false,
+            // instruct that this is primary key
             primaryKey: true,
+            // turn on auto increment
             autoIncrement: true
         },
+        // define username column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
+        // define email column
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            // don't allow duplicate emails
             unique: true,
+            // if allowNull is set to false, we can run our data through validator before creating tables
             validate: {
                 isEmail: true
             }
         },
+        // define password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
+                // means password must be at least four char long
                 len: [4]
             }
         }
@@ -46,16 +58,22 @@ User.init(
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-            // set up beforeUpdate lifecycle "hook" functionality
+            // set up beforeUpdate lifecycle hook functionality
             async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
             }
         },
+        // table config options go here
+        // pass in our imported sequelize connection (the direct connecvtion to our database)
         sequelize,
+        // don't auto create createdAt/updatedAt timestamp fields
         timestamps: false,
+        // don't pluralize name of db table
         freezeTableName: true,
+        // use underscores instead of camel case
         underscored: true,
+        // make model stay lowercase in db
         modelName: 'user'
     }
 );
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helper');
+const hbs = exphbs.create({ helpers });
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: process.env.DB_COOKIE,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
